3bba712e39abd88ad83f38951032ad0ed4e9ffcf
LeeQ <123221920+NeoNewBie@users.noreply.github.com>
2026-01-29 21:29:36 +0700

    Upload bai 9

---
+++ Bai9/Bai9/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+        Console.WriteLine("Lê Anh Quốc Msv: 2415053122234 Bài 9");
+        string[] arr = new string[]
+        {
+            "Hello",
+            null,
+            "",
+            "CSharp",
+            "   ",
+            "World"
+        };
+
+        int count = CountValidStrings(arr);
+
+        Console.WriteLine("✅ Số chuỗi khác null và khác rỗng là: " + count);
+
+    // Hàm đếm chuỗi hợp lệ
+    static int CountValidStrings(string[] arr)
+    {
+        if (arr == null)
+            return 0;
+
+        int count = 0;
+
+        foreach (string s in arr)
+        {
+            if (!string.IsNullOrEmpty(s) && !string.IsNullOrWhiteSpace(s))
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
